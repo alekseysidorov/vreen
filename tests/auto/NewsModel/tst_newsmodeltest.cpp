b5ff7266951ae354ccfb9812d6fbe960290f3271
Aleksey <gorthauer87@ya.ru>
2012-07-03 17:20:24 +0400

    Change license to LGPLv3

--- tests/auto/NewsModel/tst_newsmodeltest.cpp
+++ tests/auto/NewsModel/tst_newsmodeltest.cpp
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** VKit - vk.com API Qt bindings
+**
+** Copyright © 2012 Aleksey Sidorov <gorthauer87@ya.ru>
+**
+*****************************************************************************
+**
+** $VKIT_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+** $VKIT_END_LICENSE$
+**
+****************************************************************************/
 #include <QString>
 #include <QtTest>
 #include <QCoreApplication>
@@ -62,3 +86,4 @@ void NewsModelTest::testUpdate_data()
 QTEST_MAIN(NewsModelTest)
 
 #include "tst_newsmodeltest.moc"
+
